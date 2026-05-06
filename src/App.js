2da30056c48f9b7e2717e9dcd847da9e7bae44ae
sahithi <sahithi1234567@gmail.com>
2026-05-06 09:22:00 +0530

    fixed delete function

--- src/App.js
+++ src/App.js
@@ -4,46 +4,46 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
 
-  // GET
+  const BASE_URL = "https://todolist-backend-e30t.onrender.com/todos";
+
+  // GET TODOS
   function getTodos() {
-    fetch("https://todolist-backend-e30t.onrender.com/todos")
-      .then(res => res.json())
-      .then(data => setTodos(data))
-      .catch(err => console.log(err));
+    fetch(`${BASE_URL}/todos`)
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.log(err));
   }
 
-  // ADD
+  // ADD TODO
   function addTodo() {
-    if (text === "") return;
+    if (text.trim() === "") return;
 
-    fetch("https://todolist-backend-e30t.onrender.com/todos", {
+    fetch(`${BASE_URL}/todos`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ text: text })
     })
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(() => {
         setText("");
         getTodos();
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err));
   }
 
-  // DELETE
-  function deleteTodo(text) {
-  fetch("https://todolist-backend-e30t.onrender.com/todos", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ text: text })
-  })
-    .then(res => res.json())
-    .then(() => getTodos())
-    .catch(err => console.log(err));
-}
+  // DELETE TODO
+  function deleteTodo(id) {
+    fetch(`${BASE_URL}/todos/${id}`, {
+      method: "DELETE"
+    })
+      .then((res) => res.json())
+      .then(() => {
+        getTodos();
+      })
+      .catch((err) => console.log(err));
+  }
 
   useEffect(() => {
     getTodos();
@@ -54,17 +54,22 @@ function App() {
       <h2>Todo List</h2>
 
       <input
+        type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        placeholder="Enter todo"
       />
 
-      <button onClick={addTodo}>Add</button>
+      <button onClick={addTodo}>
+        Add
+      </button>
 
       <ul>
-        {todos.map((t, i) => (
-          <li key={i}>
+        {todos.map((t) => (
+          <li key={t._id}>
             {t.text}
-            <button onClick={() => deleteTodo(t.text)}>
+
+            <button onClick={() => deleteTodo(t._id)}>
               Delete
             </button>
           </li>
